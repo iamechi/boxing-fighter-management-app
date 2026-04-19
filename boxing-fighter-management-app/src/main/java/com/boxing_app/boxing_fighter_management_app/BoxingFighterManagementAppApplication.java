2d928b0cddfa3da974fb34d6b63efc7068439bd4
iamechi <66384457+iamechi@users.noreply.github.com>
2026-04-19 16:36:53 -0400

    Initializing fighter application

---
+++ boxing-fighter-management-app/src/main/java/com/boxing_app/boxing_fighter_management_app/BoxingFighterManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.boxing_app.boxing_fighter_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxingFighterManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxingFighterManagementAppApplication.class, args);
+	}
+
+}
---
+++ boxing-fighter-management-app/src/test/java/com/boxing_app/boxing_fighter_management_app/BoxingFighterManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boxing_app.boxing_fighter_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxingFighterManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
