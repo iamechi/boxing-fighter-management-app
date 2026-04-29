e3c15abed0cca6cefa4f97f70cc3704e0c9c1c67
iamechi <66384457+iamechi@users.noreply.github.com>
2026-04-29 00:59:22 -0400

    Adding the figher model to the class.

---
+++ boxing-fighter-management-app/src/main/java/com/boxing_app/boxing_fighter_management_app/model/Fighter.java
@@ -0,0 +1,60 @@
+package com.boxing_app.boxing_fighter_management_app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="boxers")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Fighter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long fighterID;
+
+    @Column(name="first_name")
+    private String first_name;
+
+    @Column(name="last_name")
+    private String last_name;
+
+    @Column(name="height")
+    private double height;
+
+    @Column(name="weight")
+    private double weight;
+
+    @Column(name="fights")
+    private int fights;
+
+    @Column(name="wins")
+    private int wins;
+
+    @Column(name="losses")
+    private int losses;
+
+    @Column(name="draws")
+    private int draws;
+
+    @Column(name="KOS")
+    private int KOS;
+
+    @Column(name="current_state_residence")
+    private String current_state_residence;
+
+    @Column(name="current_city_residence")
+    private String current_city_residence;
+
+    @Column(name="years_of_boxing")
+    private int years_of_boxing;
+
+    @Column(name="knockout_percentage")
+    private BigDecimal knockout_percentage;
+
+    @Column(name="photo_attachment")
+    private String photo_attachment;
+}
