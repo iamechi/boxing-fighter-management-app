c804538ebf46e91d758e4d4a9f501cbe0bf9f753
iamechi <66384457+iamechi@users.noreply.github.com>
2026-09-03 01:03:06 -0400

    Getting rid of commented out code.

--- boxing-fighter-management-app/src/main/java/com/boxing_app/boxing_fighter_management_app/model/FighterManagementAppRepository.java
+++ boxing-fighter-management-app/src/main/java/com/boxing_app/boxing_fighter_management_app/model/FighterManagementAppRepository.java
@@ -14,42 +14,4 @@ public interface FighterManagementAppRepository extends JpaRepository<Fighter, L
     @Query(value="SELECT * FROM boxers", nativeQuery = true)
     List<Fighter> AllFighters();
 
-    /*
-    @Modifying //This tells spring to use executeUpdate, which handles updates to a table instead of executeQuery, which handles querying a table
-    @Transactional//grants database transaction permissions
-    @Query(value="INSERT INTO boxers (fighterID, " +
-            "first_name, " +
-            "last_name, " +
-            "height, " +
-            "weight, " +
-            "fights, " +
-            "wins, " +
-            "losses, " +
-            "draws, " +
-            "KOS, " +
-            "current_state_residence, " +
-            "years_of_boxing, " +
-            "knockout_percentage, " +
-            "photo_attachment) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14)",
-            nativeQuery = true)
-    void addFighter(Long fighterID,
-                       String first_name,
-                       String last_name,
-                       double height,
-                       double weight,
-                       int fights,
-                       int wins,
-                       int loses,
-                       int draws,
-                       int KOS,
-                       String current_state_residence,
-                       int years_of_boxing,
-                       double knockout_percentage,
-                       String photo_attachment);
-
-     */
-
-    /*
-    Fighter updateFighters(Fighter fighter);
-     */
 }
